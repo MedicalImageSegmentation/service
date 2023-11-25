443da94d900780699e8cb0a0c51be5bc59312261
null122 <1229608477@qq.com>
2023-11-25 16:58:22 +0800

    feat: 更新aigc

--- services/aigc_service.py
+++ services/aigc_service.py
@@ -1,10 +1,16 @@
+import requests
 from google.protobuf.empty_pb2 import Empty
 
-
 from api.medicalImagesegmentation.aigc.aigc_pb2 import FileHash
-from api.medicalImagesegmentation.aigc.aigc_pb2_http import AigcServicer
+from api.medicalImagesegmentation.aigc.aigc_pb2_http import AigcServicer, register_aigc_http_server
+from core.router_register import register_fastapi_route, parse_request, parse_reply
 
 
 class AigcService(AigcServicer):
     async def ModelHandle(self, request: FileHash) -> Empty:
-        pass
+        res = requests.get("http://124.222.0.214:1234/pridict/"+request.hash)
+        print(res.url)
+        return Empty()
+
+
+register_aigc_http_server(register_fastapi_route, AigcService(), parse_request, parse_reply)
